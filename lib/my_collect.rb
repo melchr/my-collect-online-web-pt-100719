191b0c55b44924204cc179c37cfa31ebb8355c03
Melissa Christman <melchrstmn@gmail.com>
2019-10-16 16:14:54 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(collection)
+  i = 0
+  array = []
+    while i < array
+     yield(array[i])
+     i += 1
+    end
+    array
+end
 
-
+my_collect(array) do |name|
+  name.split(" ").first
+end
